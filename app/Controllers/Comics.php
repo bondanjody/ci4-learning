bc80f7f8b6c3cb8c8692476985d6b257aa97c7ba
bondanjody <bondanjodys@gmail.com>
2023-04-12 00:05:23 +0700

    Membuat KomikModel yang khusus menangani tabel komik. Membuat instansiasi nya di Controller Comic.

--- app/Controllers/Comics.php
+++ app/Controllers/Comics.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use \App\Models\KomikModel;
+
 class Comics extends BaseController
 {
     public function index()
     {
+        $komikModel = new KomikModel();
+
         $data = [
             'title'=>'Daftar Komik'
         ];
