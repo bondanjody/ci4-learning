9ebcf070ae4d9407be645287b39455d88e2fc752
bondanjody <bondanjodys@gmail.com>
2023-04-11 21:26:48 +0700

    Menambahkan navigation bar

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Contact</title>
+    <title>Bondan Web</title>
   </head>
   <body>
     <div style="background-color: tomato; text-align:center; ">
         <h1>Bondan Web</h1>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
     </div>
